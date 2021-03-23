eb01d4a683dad312f0ae389b3b7f918b14822522
Ulisses Sigma <ulisses.sigma@gmail.com>
2021-03-22 23:58:01 -0300

    :sparkles: Creation of excel file manager class.

---
+++ Managers/ExcelFileManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace adpt_xcl_tool.Managers
+{
+    public class ExcelFileManager
+    {
+
+        public void ExcelReader(string filePath)
+        {
+            if (!File.Exists(filePath)) return;
+
+        }
+    }
+}
